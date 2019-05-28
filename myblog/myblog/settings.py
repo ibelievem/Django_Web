dcfe3d755b5b95e58033b5d293893c6cb86cdcb4
xws <xws>
2019-05-28 15:05:53 +0800

    项目结束

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-
+STATIC_ROOT='/home/xws/djangoweb/Django_Web/myblog/staticroot'
 
 # 富文本配置项目
 TINYMCE_DEFAULT_CONFIG = {
