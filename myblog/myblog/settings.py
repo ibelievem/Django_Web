aba51076baa7221beafabf5c58b8c7972502cfd3
xws <xws>
2019-05-28 11:29:04 +0800

    socket连接

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2@cg04i!li0%bl@fk+04fjh03cm(bri8dtgcjf=^cyxe_)-hoo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
+#DEBUG = False
 DEBUG = True
 
-# ALLOWED_HOSTS = ["*"]
+#ALLOWED_HOSTS = ["*"]
 ALLOWED_HOSTS = []
 
 
